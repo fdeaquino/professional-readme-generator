ba337ae5c6de141393bbfca0e3da2f2138a5795e
FDeaquino <fidel.deaquino@gmail.com>
2022-10-13 13:39:34 -0500

    changed licences Type to -List-, modified messages/console log

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // TODO: Include packages needed for this application
-//Should I use VAR(from inquirer 8.2.4 documentation) or CONST (bc instructor said to stop using var)??
 const inquirer = require('inquirer');
 const generateMarkdown = require('./develop/utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
+// Some of these prompts are straight from the portfolio-generator from online module
 const questions = [
     {
         type: 'input',
@@ -18,13 +18,13 @@ const questions = [
                 return false;
             }
         }
-    }, 
+    },
     {
         type: 'input',
         name: 'email',
-        message: "What your email?",
-        validate: nameInput => {
-            if (nameInput) {
+        message: "What is your email?",
+        validate: emailInput => {
+            if (emailInput) {
                 return true;
             } else {
                 console.log("Please enter email.");
@@ -34,10 +34,10 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'name', //should the value be title since line16 in generateMarkdown.js is 'data.title'?
+        name: 'title', 
         message: "What is the name of your project?",
-        validate: nameInput => {
-            if (nameInput) {
+        validate: titleInput => {
+            if (titleInput) {
                 return true;
             } else {
                 console.log("Please enter your project's name.");
@@ -88,10 +88,10 @@ const questions = [
     },
     {
         // Come back to revise license information (Should this be checkboxes?)
-        type: 'checkbox',
+        type: 'list',
         name: 'licenses',
-        message: 'What license does your project have? (Choose one)',
-        choices: ['MIT', 'GPL 3.0', 'Apache 2.0', 'BSD 3', 'None']
+        message: 'What license does your project have? (Please choose one)',
+        choices: ['MIT', 'GPL: General Public Use License', 'Apache License 2.0', 'ISC', 'None']
     },
     {
         // Come back to Contribution guidelines
@@ -111,7 +111,7 @@ const questions = [
         // Come back to Tests
         type: 'input',
         name: 'tests',
-        message: 'What is the test process for this project? (Required)', 
+        message: 'What is the test process for this project? (Required)',
         validate: testsInput => {
             if (testsInput) {
                 return true;
@@ -125,19 +125,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
 // TODO: Create a function to initialize app
+// 
 function init() {
-   return inquirer.prompt(questions)
-    .then((data) => {
-        console.log(data);
-        return data
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    // console.log('ReadMe file created! Check out README.md in this directory to see it!');
+    return inquirer.prompt(questions)
+        .then((data) => {
+            const mark = generateMarkdown(data);
+            fs.writeFile('./develop/README.md', mark, err => {
+                if (err) {
+                    console.log('Could not save README.md file')
+                } else {
+                    console.log('README.md file created successfully and can be found inside the develop folder.')
+                }
+            })
+            console.log(data);
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
 // Function call to initialize app
@@ -153,27 +159,28 @@ init();
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
+        //  DONE
 
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
-
+        //  DONE
 
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
+        //  DONE
 
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
+        //  NEED HELP!!
 
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
+        //  DONE
 
 // WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
+        //  DONE
 
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
+        //  DONE
