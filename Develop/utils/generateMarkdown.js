f14c9fca5763563b1898ff965dbf5e22ea9293c4
FDeaquino <fidel.deaquino@gmail.com>
2022-10-13 13:34:35 -0500

    updated generateMarkdown function, added license information and badges - not functioning yet"

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,53 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenses) {
+  if(licenses === 'MIT') {
+    return '![MIT License](https://img.shields.io/badge/license-MIT-brightgreen)';
+  } else if (licenses === 'GPL: General Public Use License') {
+    return '![GPL License](https://img.shields.io/badge/license-GPL-yellow)';
+  } else if (licenses === 'Apache License 2.0'){
+    return '![Apache License](https://img.shields.io/badge/license-Apache--2.0-blue)';
+  } else if (licenses === 'ISC') {
+    return '![ISC License](https://img.shields.io/badge/license-ISC-orange)';
+  } else {
+    return '';
+  }
+ }
+
+ 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenses) {
+  if (licenses === 'MIT') {
+    return 'https://mit-license.org/';
+  } else if (licenses === 'GPL: General Public Use License') {
+    return 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+  } else if (licenses === 'Apache License 2.0') {
+    return 'http://www.apache.org/licenses/LICENSE-2.0';
+  } else if (licenses === 'ISC') {
+    return 'https://www.isc.org/licenses/';
+  } else {
+    return '';
+  }
+ }
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenses) {
+  if (licenses != 'None') {
+    return `
+## License
+[${licenses}](${renderLicenseLink(licenses)})`;
+  } else {
+    return '';
+  }
+ }
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
-  ${data.username}
+  return `# ${data.title}
 
-  ## Table of Contents
-  -[Project Description](#Description)
-  -[Installation](#Installation)
-  -[Usage](#Usage)
-  -[Licenses](#Licenses) 
-  -[Contribution](#Contribution)
-  -[Testing](#Testing)
+## Table of Contents
+- [Project Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#licenses) 
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Questions](#questions)
 
-  ## Description
-  ${data.description}
-  
-  ## Installation
-  ${data.installation}
-  
-  ## Usage
-  ${data.usage}
+## Project Description
+${data.description}
 
-  ## Licenses
-  ${data.licenses}
+## Installation
+${data.installation}
 
-  ## Contribution
-  ${data.contribution}
+## Usage
+${data.usage}
 
-  ## Testing
-  ${data.tests}
+## License
+${data.licenses}
 
-  ## Questions
-  ${data.email}
-  ${data.github}
+## Contribution
+${data.contribution}
 
-  ##__
-  ${__}
+## Testing
+${data.tests}
+
+## Questions
+If you have questions about this project, please feel free to reach me via [email](${data.email}). ${data.email}
+
+Explore more of my projects on [GitHub](https://github.com/${data.github})
+
 `;
 }
 
